37bfea18f01b50fa3c426f322fcef396d9ec4672
LewisP <33624714+LewisPercival@users.noreply.github.com>
2020-09-16 17:20:53 +0100

    #6 resolve name for claims principal (#7)

--- src/Easify.Ef.UnitTests/Extensions/PrincipalExtensionsTests.cs
+++ src/Easify.Ef.UnitTests/Extensions/PrincipalExtensionsTests.cs
@@ -1,20 +1,22 @@
 // This software is part of the Easify.Ef Library
 // Copyright (C) 2018 Intermediate Capital Group
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Security.Principal;
 using Easify.Ef.Extensions;
 using FluentAssertions;
@@ -38,6 +40,20 @@ namespace Easify.Ef.UnitTests.Extensions
 
             // Assert
             actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Should_GetUserName_ReturnTheCorrectUserFromClaimsPrincipal()
+        {
+            //Arrange
+            var identity = new ClaimsIdentity(new[] {new Claim(ClaimTypes.Name, "n/a")}, "Federated", ClaimTypes.NameIdentifier, ClaimTypes.Role);
+            var claimsPrincipal = new ClaimsPrincipal(identity);
+
+            //Act
+            var actual = claimsPrincipal.GetUserName();
+
+            //Assert
+            actual.Should().Be("n/a");
         }
     }
 }
